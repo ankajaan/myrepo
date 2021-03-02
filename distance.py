033efefe1d1ac954d6d5c381f12653ca28e3b113
ankajaan <ankaja.anilkumar@gmail.com>
2021-03-02 09:36:33 +0530

    Initial Commit

---
+++ distance.py
@@ -0,0 +1,9 @@
+x1= int(input("enter the x1 co-ordinate"))
+y1= int(input("enter the y1 co-ordinate"))
+
+x2= int(input("enter the x2 co-ordinate"))
+y2= int(input("enter the y2 co-ordinate"))
+
+distance = ((x1-x2)**2 +(y1-y2)**2)**0.5
+
+print('The distance between two given points: %d units' %(distance))
